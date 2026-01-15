a065100502bcfaa1533a4ff1cf515b039dff5dd1
faisal-din <dahrifaisal56@gmail.com>
2026-01-15 16:23:12 +0500

    updated home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,31 @@
 import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
 const Home = async () => {
   const session = await auth();
 
   return (
     <>
-      <h1 className="">Welcome to the world of Next.js</h1>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3" asChild>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+
+      <section className="mt-11">
+        <p>Local Search</p>
+      </section>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+        <p>Question Card 4</p>
+      </div>
     </>
   );
 };
