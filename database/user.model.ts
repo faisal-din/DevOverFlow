610e5154ff3ebb663f22e6873e19186c922f313c
faisal-din <dahrifaisal56@gmail.com>
2026-01-17 23:36:14 +0500

    created user model

---
+++ database/user.model.ts
@@ -0,0 +1,32 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IUser {
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String },
+    image: { type: String, required: true },
+    location: { type: String },
+    portfolio: { type: String },
+    reputation: { type: Number, default: 0 },
+  },
+  { timestamps: true }
+);
+
+const UserModel = models.user || model<IUser>("User", UserSchema);
+
+export default UserModel;
