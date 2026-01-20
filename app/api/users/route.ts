cd7568548a0d28d3937d9d1edd5445fac4b94920
faisal-din <dahrifaisal56@gmail.com>
2026-01-20 15:47:17 +0500

    Implement User by Email

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,7 +4,6 @@ import { ValidationError } from "@/lib/http-errors";
 import dbConnect from "@/lib/mongoose";
 import { UserSchema } from "@/lib/validations";
 import { APIErrorResponse } from "@/types/global";
-import { error } from "console";
 import { NextResponse } from "next/server";
 
 // GET /api/users - Retrieve a list of all users
