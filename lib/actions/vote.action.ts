3f2964bc846cf5f8a419f7b66858261286983fc9
faisal-din <dahrifaisal56@gmail.com>
2026-02-10 22:04:31 +0500

    Implment Create Vote Server Actions

---
+++ lib/actions/vote.action.ts
@@ -0,0 +1,95 @@
+"use server";
+
+import mongoose, { ClientSession } from "mongoose";
+import { AnswerModel, QuestionModel, VoteModel } from "@/database";
+
+import { CreateVoteParams, UpdateVoteCountParams } from "@/types/action";
+import action from "../handlers/action";
+
+import handleError from "../handlers/error";
+import { ActionResponse, ErrorResponse } from "@/types/global";
+import { CreateVoteSchema, UpdateVoteCountSchema } from "../validations";
+
+export async function updateVoteCountAction(
+  params: UpdateVoteCountParams,
+  session?: ClientSession
+): Promise<ActionResponse> {
+  const validationResult = await action({
+    params,
+    schema: UpdateVoteCountSchema,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { targetId, targetType, voteType, change } = validationResult.params!;
+
+  const Model = targetType === "question" ? QuestionModel : AnswerModel;
+  const voteField = voteType === "upvote" ? "upvotes" : "downvotes";
+
+  try {
+    const result = await Model.findByIdAndUpdate({ _id: targetId }, { $inc: { [voteField]: change } }, { session });
+
+    if (!result) return handleError(new Error("Failed to update vote count")) as ErrorResponse;
+
+    return { success: true };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
+
+export async function CreateVoteAction(params: CreateVoteParams): Promise<ActionResponse> {
+  const validationResult = await action({
+    params,
+    schema: CreateVoteSchema,
+    authorize: true,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { targetId, targetType, voteType } = validationResult.params!;
+  const userId = validationResult.session?.user?.id;
+
+  if (!userId) handleError(new Error("Unauthorized")) as ErrorResponse;
+
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
+  try {
+    const existingVote = await VoteModel.findOne({
+      author: userId,
+      actionId: targetId,
+      actionType: targetType,
+    }).session(session);
+
+    if (existingVote) {
+      if (existingVote.voteType === voteType) {
+        // If the user has already voted with the same voteType, remove the vote
+        await VoteModel.deleteOne({ _id: existingVote._id }).session(session);
+        await updateVoteCountAction({ targetId, targetType, voteType, change: -1 }, session);
+      } else {
+        // If the user has already voted with a different voteType, update the vote
+        await VoteModel.findByIdAndUpdate(existingVote._id, { voteType }, { new: true, session });
+        await updateVoteCountAction({ targetId, targetType, voteType, change: 1 }, session);
+      }
+    } else {
+      // If the user has not voted yet, create a new vote
+      await VoteModel.create([{ targetId, targetType, voteType, change: 1 }], {
+        session,
+      });
+      await updateVoteCountAction({ targetId, targetType, voteType, change: 1 }, session);
+    }
+
+    await session.commitTransaction();
+    session.endSession();
+
+    return { success: true };
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    return handleError(error) as ErrorResponse;
+  }
+}
