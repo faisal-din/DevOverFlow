637930a4fbfb29cfe3d65f08343534037136e46f
faisal-din <dahrifaisal56@gmail.com>
2026-02-11 21:53:33 +0500

    Implement Add to Collection Action

---
+++ lib/actions/collection.action.ts
@@ -0,0 +1,64 @@
+"use server";
+
+import { CollectionBaseParams } from "@/types/action";
+import { ActionResponse, ErrorResponse } from "@/types/global";
+import { CollectionBaseSchema } from "../validations";
+import action from "../handlers/action";
+import handleError from "../handlers/error";
+import { CollectionModel, CollectionModel, QuestionModel } from "@/database";
+import { revalidatePath, revalidatePath } from "next/cache";
+import ROUTES from "@/constants/routes";
+
+export async function toggleSaveQuestionAction(
+  params: CollectionBaseParams
+): Promise<ActionResponse<{ saved: boolean }>> {
+  const validationResult = await action({
+    params,
+    schema: CollectionBaseSchema,
+    authorize: true,
+  });
+
+  if (validationResult instanceof Error) {
+    return handleError(validationResult) as ErrorResponse;
+  }
+
+  const { questionId } = validationResult.params!;
+  const userId = validationResult.session?.user?.id;
+
+  try {
+    const question = await QuestionModel.findById(questionId);
+    if (!question) throw new Error("Question not found");
+
+    const collection = await CollectionModel.findOne({
+      question: questionId,
+      author: userId,
+    });
+
+    if (collection) {
+      await CollectionModel.findByIdAndDelete(collection.id);
+
+      return {
+        success: true,
+        data: {
+          saved: false,
+        },
+      };
+    }
+
+    await CollectionModel.create({
+      question: questionId,
+      author: userId,
+    });
+
+    revalidatePath(ROUTES.QUESTION(questionId));
+
+    return {
+      success: true,
+      data: {
+        saved: true,
+      },
+    };
+  } catch (error) {
+    return handleError(error) as ErrorResponse;
+  }
+}
