b1c164f4fe430b784cbc4f37709bfdd7771f287a
faisal-din <dahrifaisal56@gmail.com>
2026-02-15 12:28:39 +0500

    Implement Edit and Delete Buttons UI

---
+++ components/user/EditDeleteAction.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { toast } from "sonner";
+
+interface Props {
+  type: string;
+  itemId: string;
+}
+
+const EditDeleteAction = ({ type, itemId }: Props) => {
+  const router = useRouter();
+
+  const handleEdit = async () => {
+    router.push(`/questions/${itemId}/edit`);
+  };
+
+  const handleDelete = async () => {
+    if (type === "Question") {
+      // Call API to delete question
+
+      toast.success("Question deleted", {
+        description: "Your question has been deleted successfully.",
+      });
+    } else if (type === "Answer") {
+      // Call API to delete answer
+
+      toast.success("Answer deleted", {
+        description: "Your answer has been deleted successfully.",
+      });
+    }
+  };
+
+  return (
+    <div className={`flex items-center justify-end gap-3 max-sm:w-full ${type === "Answer" && "justify-center gap-0"}`}>
+      {type === "Question" && (
+        <Image
+          src="/icons/edit.svg"
+          alt="edit"
+          width={14}
+          height={14}
+          className="cursor-pointer object-contain"
+          onClick={handleEdit}
+        />
+      )}
+
+      <AlertDialog>
+        <AlertDialogTrigger className="cursor-pointer">
+          <Image src="/icons/trash.svg" alt="trash" width={14} height={14} />
+        </AlertDialogTrigger>
+        <AlertDialogContent className="background-light800_dark300">
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your{" "}
+              {type === "Question" ? "question" : "answer"} and remove it from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className="btn cursor-pointer">Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              className="border-primary-100! bg-primary-500! text-light-800! cursor-pointer"
+              onClick={handleDelete}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default EditDeleteAction;
